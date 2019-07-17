cba0a090cf295612d0ec1aaebef971302f98a4de
Fraxeum <52670279+Fraxeum@users.noreply.github.com>
2019-07-17 09:57:26 +0200

    Multichain Version Update

--- fraxeum-node-v01/node-linux/cookbooks/multi_chain/attributes/default.rb
+++ fraxeum-node-v01/node-linux/cookbooks/multi_chain/attributes/default.rb
@@ -2,7 +2,7 @@ default["app"]["directory"] = "/apps"
 default["data"]["directory"] = "/datadir"
 default["temp"]["directory"] = "/tmp"
 
-default["software"]["multichain"] = "multichain-2.0-beta-1.tar.gz"
+default["software"]["multichain"] = "multichain-2.0.2.tar.gz"
 default["blockchain"]["name"] = "FraxTest"
 default["blockchain"]["ip"] = "95.179.167.94"
 default["blockchain"]["port"] = "9717"
