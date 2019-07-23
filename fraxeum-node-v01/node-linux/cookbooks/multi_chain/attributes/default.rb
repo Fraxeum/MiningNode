b1c2b64df75692cdd7a5f7b133eee03709ee69f1
Fraxeum <52670279+Fraxeum@users.noreply.github.com>
2019-07-23 11:43:20 +0200

    TestNet - Test 2

--- fraxeum-node-v01/node-linux/cookbooks/multi_chain/attributes/default.rb
+++ fraxeum-node-v01/node-linux/cookbooks/multi_chain/attributes/default.rb
@@ -3,6 +3,6 @@ default["data"]["directory"] = "/datadir"
 default["temp"]["directory"] = "/tmp"
 
 default["software"]["multichain"] = "multichain-2.0.2.tar.gz"
-default["blockchain"]["name"] = "FraxTest"
+default["blockchain"]["name"] = "FraxTest2"
 default["blockchain"]["ip"] = "95.179.167.94"
-default["blockchain"]["port"] = "9717"
+default["blockchain"]["port"] = "9727"
