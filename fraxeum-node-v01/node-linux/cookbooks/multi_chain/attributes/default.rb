d7ee7b0fcc10c7738bbb5ad563f74417ae678b1a
Fraxeum <52670279+Fraxeum@users.noreply.github.com>
2019-08-06 15:36:32 +0200

    Update default.rb

--- fraxeum-node-v01/node-linux/cookbooks/multi_chain/attributes/default.rb
+++ fraxeum-node-v01/node-linux/cookbooks/multi_chain/attributes/default.rb
@@ -4,5 +4,5 @@ default["temp"]["directory"] = "/tmp"
 
 default["software"]["multichain"] = "multichain-2.0.2.tar.gz"
 default["blockchain"]["name"] = "TestNet1.02"
-default["blockchain"]["ip"] = "95.179.167.94"
-default["blockchain"]["port"] = "9727"
+default["blockchain"]["ip"] = "136.244.106.52"
+default["blockchain"]["port"] = "9737"
